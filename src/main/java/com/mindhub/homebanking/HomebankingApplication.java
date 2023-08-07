3c27f3e37e13c6a519842e75acec54065333fe7e
roxana2275 <roxana.ciccone@gmail.com>
2023-08-07 17:36:10 -0300

    finalizado task-2 con correccion de fecha

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,11 +1,15 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -15,15 +19,28 @@ public class HomebankingApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(ClientRepository clientRepository){
+	public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository){
 		;
 		return(args -> {
-			Client client1 = new Client("Melba","Morel","melba@mindhub.com");
-			clientRepository.save(client1);
+					LocalDate date = LocalDate.now();
+					Account account1 = new Account("VIN001",date,5000);
+					Account account2 = new Account("VIN002",date.plusDays(1),7500);
+					Client client1 = new Client("Melba", "Morel","melba@melba");
+					clientRepository.save(client1);
+					client1.addAccount(account1);
+					client1.addAccount(account2);
+					accountRepository.save(account1);
+					accountRepository.save(account2);
+					clientRepository.save(client1);
 
-			Client client2= new Client("Ricardo","Sanchez","ricardo@mindhub.com");
-			clientRepository.save(client2);
 
+
+					Account account3 = new Account( " VIN003",date, (double) 10000);
+					Client client2= new Client("Ricardo","Sanchez","ricardo@mindhub.com");
+					clientRepository.save(client2);
+					client2.addAccount(account3);
+					accountRepository.save(account3);
+					clientRepository.save(client2);
 
 		});
 	}
---
+++ src/main/java/com/mindhub/homebanking/models/Account.java
@@ -0,0 +1,69 @@
+package com.mindhub.homebanking.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String number;
+
+    private LocalDate creationDate;
+    private double balance;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="client_id")
+    private Client client;
+    public Account() {
+    }
+
+    public Account(String number, LocalDate creationDate, double balance) {
+        this.number = number;
+        this.creationDate = creationDate;
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @JsonIgnore
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
+
